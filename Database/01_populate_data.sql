9836b31a9bb0026ab0563a2a9e1719bf79a2453b
Aaron D. Morris <rndmorris@gmail.com>
2018-04-10 19:00:25 -0500

    Add download screen and new sample data

--- Database/01_populate_data.sql
+++ Database/01_populate_data.sql
@@ -11,6 +11,14 @@ INSERT INTO VisibilitySetting VALUES
 CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 1','This is a test dictionary.','public',1);
 CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 2','This is a test dictionary.',' puBliC ',1);
 CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 3','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 4','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 5','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 6','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 7','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 8','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 9','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 10','This is a test dictionary.','PUBLIC',1);
+CALL rprtr_DictionaryDefinition_AddNew('Test Dictionary 11','This is a test dictionary.','PUBLIC',1);
 
 CALL rprtr_DictionaryVersion_AddNew(1,1);
 CALL rprtr_DictionaryVersion_AddNew(1,1);
@@ -18,3 +26,11 @@ CALL rprtr_DictionaryVersion_AddNew(2,1);
 CALL rprtr_DictionaryVersion_AddNew(2,1);
 CALL rprtr_DictionaryVersion_AddNew(2,1);
 CALL rprtr_DictionaryVersion_AddNew(3,1);
+CALL rprtr_DictionaryVersion_AddNew(4,1);
+CALL rprtr_DictionaryVersion_AddNew(5,1);
+CALL rprtr_DictionaryVersion_AddNew(6,1);
+CALL rprtr_DictionaryVersion_AddNew(7,1);
+CALL rprtr_DictionaryVersion_AddNew(8,1);
+CALL rprtr_DictionaryVersion_AddNew(9,1);
+CALL rprtr_DictionaryVersion_AddNew(10,1);
+CALL rprtr_DictionaryVersion_AddNew(11,1);
